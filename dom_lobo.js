7b50379d7635d6bc5b58f60579750f3abfb8411a
Gustavolobopiedade <gustavo.piedade@escola.pr.gov.br>
2025-09-16 09:34:59 -0300

    svsdfsfs

--- dom_lobo.js
+++ dom_lobo.js
@@ -2,8 +2,31 @@ const rodri_circulo =document.getElementById('circulo')
 let tamanho_rodri = 10
 
 rodri_circulo.addEventListener('dblclick',()=>{
+    alert(rodri_tamanho)
     rodri_circulo.style.borderRadius = `${tamanho_rodri}px`
-    rodri_circulo.style.width = `${tamanho_rodri}px`
-    rodri_circulo.style.height = `${tamanho_rodri}px`
-    tamanho_rodri = tamanho_rodri+20
+    rodri_circulo.style.width = `${tamanho_rodri*2}px`
+    rodri_circulo.style.height = `${tamanho_rodri*2}px`
+    rodri_tamanho+= 30;
+})
+
+function mudarCor(novaCor){
+    var elemento = document.getElementById("para1")
+    elemento.style.backgroundColor = novaCor
+    elemento.style.display = 'inline';
+};
+
+const imagem = document.getElementById('foto')
+let foto_t = false
+
+
+imagem.addEventListener('click',()=>{
+    if(foto_t === false){
+        imagem.src = 'pokedex.png';
+        foto_t = true
+    }else{
+        imagem.src = 'teste.jpg'
+        foto_t = false;
+        
+
+    }
 })
